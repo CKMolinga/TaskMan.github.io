cb3e3785fe95a8e1b9114708e50ce1383a512653
CKMolinga <ck.molinga@gmail.com>
2022-04-28 00:46:10 +0200

    added logout composable

---
+++ src/composables/useLogout.js
@@ -0,0 +1,23 @@
+import { ref } from 'vue'
+import { dbAuth } from '../firebase/config'
+
+const error = ref(null)
+
+const logout = async() => {
+    error.value = null
+
+    try {
+        await dbAuth.signOut()
+        error.value = null
+
+    } catch (err) {
+        console.log(err.message)
+        error.value = err.message
+    }
+}
+
+const useLogout = () => {
+    return { error, logout }
+}
+
+export default useLogout

6aeaf091b7dfe7e88da46cb8364288a8046e20d7
CKMolinga <ck.molinga@gmail.com>
2022-05-20 17:36:43 +0200

    added responsive styles

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -32,6 +32,40 @@
 
       <main class="left-flex">
 
+          <div class="hamburger-menu">
+                <span class="material-icons hamburger-menu" @click="toggleMenu">menu</span>
+          </div>
+
+          <div class="dropDown-menu">
+                <ul class="dropDown-menu__list">
+                    <li class="dropDown-menu__item">
+                        <router-link to="/dashboard" class="dropDown-menu__link">
+                            <span>Dashboard</span>
+                        </router-link>
+                    </li>
+                    <li class="dropDown-menu__item">
+                        <router-link to="/add-member" class="dropDown-menu__link">
+                            <span>Add Member</span>
+                        </router-link>
+                    </li>
+                    <li class="dropDown-menu__item">
+                        <router-link to="/notifications" class="dropDown-menu__link">
+                            <span>Notifications</span>
+                        </router-link>
+                    </li>
+                    <li class="dropDown-menu__item">
+                        <router-link to="/settings" class="dropDown-menu__link">
+                            <span>Settings</span>
+                        </router-link>
+                    </li>
+                    <li class="dropDown-menu__item">
+                        <router-link to="/logout" class="dropDown-menu__link">
+                            <span>Logout</span>
+                        </router-link>
+                    </li>
+                </ul>
+          </div>
+
           <section class="top">
               <div class="add-task">
                   <router-link to="/add-task">Add Task</router-link>
@@ -190,6 +224,12 @@ export default {
             taskToBeDeleted = {}; 
         }
 
+        // toggle menu
+        const toggleMenu = () => {
+            const menu = document.querySelector('.dropDown-menu')
+            menu.classList.toggle('show-menu')
+        }
+
          const callDeleteTask = (task) => {
             const modal = document.querySelector('.overlay')
             modal.classList.toggle('show-modal')
@@ -204,7 +244,7 @@ export default {
              toggleModal()
         }
 
-        return { tasks, task, error, taskError, user, handleClick, handleDelete, toggleModal,callDeleteTask, computedTask, taskToBeDeleted, members, memberError, memberLoad }
+        return { tasks, task, error, taskError, user, handleClick, handleDelete, toggleMenu, toggleModal,callDeleteTask, computedTask, taskToBeDeleted, members, memberError, memberLoad }
 
     }
 
@@ -298,6 +338,9 @@ main.left-flex {
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
     overflow-y: scroll;
 }
+div.dropDown-menu {
+    display: none;
+}
 section.top {
     display: flex;
     flex-direction: row;
@@ -310,6 +353,9 @@ section.top {
     position: sticky;
     top: 0;
     z-index: 100;
+}
+div.hamburger-menu {
+    display: none;
 }
 .add-task {
     display: flex;
@@ -531,6 +577,186 @@ span.close {
 .delete-btn {
     background-color: red;
 }
-    
+
+@media (max-width: 767.98px){
+    nav.sidebar {
+        display: none;
+    }
+    main.left-flex {
+        width: 100%;
+    }
+    div.hamburger-menu {
+    display: flex;
+    padding: 0.5rem 1rem;
+}
+div.show-menu {
+    display: flex;
+    flex-direction: column;
+}
+section.top {
+    display: none;
+}
+span.hamburger-menu {
+    font-size: 2rem;
+    cursor: pointer;
+}
+ul {
+    padding: 0;
+    margin: 0;
+}
+ul.dropDown-menu__list li {
+    list-style-type: none;
+    padding: 0.5rem 1rem;
+}
+ul.dropDown-menu__list li a {
+    color: #000;
+    /* border-bottom: 1px solid #292F4C; */
+}
+}
+
+/* Responsive-Design */
+/* @media screen and (max-width: 768px) {
+    .task-group {
+        flex-direction: column;
+    }
+    .task-group span {
+        margin-right: 0;
+    }
+    .task-group h3 {
+        margin-right: 0;
+    }
+    .task-group-select {
+        display: none;
+    }
+    .tasks-table {
+        flex-direction: column;
+    }
+    .tasks-table table {
+        width: 100%;
+    }
+    .tasks-table table th {
+        padding: 0.5rem;
+        font-weight: bold;
+        text-align: left;
+        background: #f1f1f1;
+        color: #292F4C;
+        border: 1px solid #E5E5E5;
+    }
+    .tasks-table table td {
+        text-align: left;
+        border: 1px solid #E5E5E5;
+        padding: 1rem;
+    }
+    td .material-icons {
+        font-size: 1.5rem;
+        margin-right: 0.5rem;
+        cursor: pointer;
+    }
+    td .assign-new-member {
+        font-size: 1.5rem;
+        margin-left: 1.5rem;
+        cursor: pointer;
+    }
+    td.flex-description {
+        display: flex;
+        justify-content: space-between;
+        border-bottom: 0;
+        border-right: 0;
+        border-left: 0;
+    }
+    .task-group-select select {
+        width: 100%;
+        outline: none;
+        background: transparent;
+        border: none;
+        color: #fff;
+        font-size: 1rem;
+        font-weight: bold;
+    }
+    select option {
+        color: #292F4C;
+    }
+    a {
+        text-decoration: none;
+        color: #fff;
+    }
+    span.delete {
+        color: rgba(212, 41, 41, 0.938);
+    }
+    .overlay {
+        display: none;
+    }
+    .modal {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 80%;
+        max-width: 500px;
+        background-color: #fff;
+        z-index: 101;
+        padding: 30px;
+        border-radius: 5px;
+        color: #000;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+    .modal-content {
+        height: auto;
+        border-radius: 0.2rem;
+        padding: 1rem;
+    }
+    .modal-header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+    .modal-header a {
+        text-decoration: none;
+    }
+    span.close {
+        position: absolute;
+        right: 20px;
+        top: 10px;
+        color: #292F4C;
+        padding: 0.5rem;
+        font-size: 1.5rem;
+        font-weight: bold;
+        cursor: pointer;
+        border-radius: 50%;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        background-color: transparent;
+        cursor: pointer;
+    }
+    .modal-body {
+        padding: 1rem;
+    }
+    .modal-body h3 {
+        font-size: 1.5rem;
+        text-align: center;
+    }
+    .modal-footer {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0.5rem 1rem;
+    }
+    .logout-btn {
+        padding: 0.8rem 1.5rem;
+        color: #fff;
+        background-color: #292F4C;
+        border-radius: 0.2rem;
+        cursor: pointer;
+        outline: none;
+        border: none;
+    }
+    .logout-btn:hover {
+        transform: scale(1.1);
+    }
+    .delete-btn {
+        background-color: red;
+    }
+} */
+
 
 </style>
